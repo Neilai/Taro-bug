21c2efe2628340f05f5297394e7878732b696047
laijingzhi <laijingzhi@bytedance.com>
2020-08-12 10:29:05 +0800

    canvas bug

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,45 +1,29 @@
-import { View, Text } from "@tarojs/components";
+import { View, Canvas ,Image} from "@tarojs/components";
 import React, { Component } from "react";
 import Taro from "@tarojs/taro";
 import "./index.scss";
 
 export default class Index extends Component {
-  state = {
-    count: 0,
-  };
-  componentDidMount() {
-    Taro.createSelectorQuery()
-      .select("#testId")
-      .boundingClientRect((rec) => {
-        console.log("rec", rec);
-      })
-      .exec();
-  }
+  private ctx;
 
-  componentDidUpdate() {
-    Taro.createSelectorQuery()
-      .select("#testId")
-      .boundingClientRect((rec) => {
-        console.log("rec", rec);
-      })
-      .exec();
+  onReady() {
+    this.ctx = Taro.createCanvasContext("image-cropper");
+    this.ctx.setFillStyle("red");
+    this.ctx.fillRect(0, 0, 100, 100);
+    this.ctx.draw(false, () => {
+      console.log("call back");
+    });
   }
 
   render() {
     return (
-      <View className='xxx'>
-        <Text
-          id='testId'
-          onClick={() => {
-            this.setState({
-              count: this.state.count + 1,
-            });
-          }}
-        >
-          Hello world!
-          <br></br>
-          {this.state.count}
-        </Text>
+      <View>
+        <Canvas
+          canvasId='image-cropper'
+          disable-scroll='true'
+          style='width: 100px;height:100px'
+          className='image-cropper-canvas'
+        />
       </View>
     );
   }
